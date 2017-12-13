566f6f2fd63e7a527035380050f8b16e1e411784
ivankremlyov <ivankremlyov@mail.ru>
2017-12-13 09:42:59 +0700

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def sigmoid(x):
-	return 1.0/(1.0 + np.exp(-x))
+def tanh(x):
+	return (np.exp(2*x)-1)/(np.exp(2*x)+1)
 
-def sigmoid_der(x):
-	return x*(1.0 - x)
+def tanh_der(x):
+	return 4/((np.exp(x)+np.exp(-x))**2)
 
 number_of_neurons = int(input("Please, enter the number of neurons "))
 number_of_layers = int(input("Please, enter the number of layers ")) 
@@ -12,41 +12,39 @@ number_of_layers = int(input("Please, enter the number of layers "))
 class NN:
     def __init__(self, inputs):
         self.inputs = inputs
-        self.l=len(self.inputs)
-        self.li=len(self.inputs[0])
+        self.l = len(self.inputs)
+        self.li = len(self.inputs[0])
 
-        self.wi=np.random.random((self.li, self.l))
-        self.wh=np.random.random((self.l, 1))
+        self.wi = np.random.random((self.li, self.l))
+        self.wh = np.random.random((self.l, 1))
 
         
 
     def generate_weights(self):
-        """ generate random weight values based on data lenght """
-        self.weights = [rd.random() for i in range(0, self.lenght)]
+        self.weights = [np.random.uniform(0,1) for i in range(0, self.lenght)]
 
     def get_weights(self):
-        """ return weights values """
         print(self.weights)
         
     def think(self, inp):
-        s1=sigmoid(np.dot(inp, self.wi))
-        s2=sigmoid(np.dot(s1, self.wh))
+        s1 = tanh(np.dot(inp, self.wi))
+        s2 = tanh(np.dot(s1, self.wh))
         return s2
 
-    def train(self, inputs,outputs, it, learning_rate=30):
+    def train(self, inputs,outputs, it):
         for i in range(it):
             l0 = inputs
-            l1 = sigmoid(np.dot(l0, self.wi))
-            l2 = sigmoid(np.dot(l1, self.wh))
+            l1 = tanh(np.dot(l0, self.wi))
+            l2 = tanh(np.dot(l1, self.wh))
 
             l2_err = outputs - l2
-            l2_delta = np.multiply(l2_err, sigmoid_der(l2))
+            l2_delta = np.multiply(l2_err, tanh_der(l2))
 
-            l1_err=np.dot(l2_delta, self.wh.T)
-            l1_delta=np.multiply(l1_err, sigmoid_der(l1))
+            l1_err = np.dot(l2_delta, self.wh.T)
+            l1_delta = np.multiply(l1_err, tanh_der(l1))
 
-            self.wh+= learning_rate * np.dot(l1.T, l2_delta)
-            self.wi+= learning_rate * np.dot(l0.T, l1_delta)
+            self.wh+= np.dot(l1.T, l2_delta)
+            self.wi+= np.dot(l0.T, l1_delta)
             
            
 
@@ -55,11 +53,11 @@ class NN:
         deltas = []
 
         error = target - self.layers[-1]
-        delta = error*dsigmoid(self.layers[-1])
+        delta = error*tanh_der(self.layers[-1])
         deltas.append(delta)
 
         for i in range(len(self.shape)-2,0,-1):
-            delta = np.dot(deltas[0],self.weights[i].T)*dsigmoid(self.layers[i])
+            delta = np.dot(deltas[0],self.weights[i].T)*tanh_der(self.layers[i])
             deltas.insert(0,delta)
             
         for i in range(len(self.weights)):
@@ -78,6 +76,8 @@ outputs=np.array([[0,1,1,0]]).T
 n=NN(inputs)
 print(n.think(inputs))
 n.train(inputs, outputs, 20000)
-print(n.think(inputs))
-
+a = (n.think(inputs))
+a[a > 0.5] = 1
+a[a < 0.5] = 0
+print(a)
 
